793ac27e610b89c4260af55cdbf8098d0c46153d
Amyas Merivale <merivale@users.noreply.github.com>
2019-09-13 14:01:40 +0200

    added source map support

--- webpack.browser.js
+++ webpack.browser.js
@@ -1,32 +1,30 @@
 const path = require('path')
 
-module.exports = function (env, argv) {
-  return {
-    entry: './src/browser/index.js',
-    // devtool: 'inline-source-map',
-    devServer: {
-      contentBase: path.join(__dirname, 'browser')
-    },
-    output: {
-      filename: 'TurtleSystemE.js',
-      path: path.resolve(__dirname, 'browser')
-    },
-    resolve: {
-      alias: {
-        common: path.resolve(__dirname, 'src/common/')
-      }
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: ['style-loader', 'css-loader', 'sass-loader']
-        },
-        {
-          test: /\.t(bas|pas|py|gx)$/,
-          use: 'raw-loader'
-        }
-      ]
+module.exports = {
+  entry: './src/browser/index.js',
+  // devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'browser')
+  },
+  output: {
+    filename: 'TurtleSystemE.js',
+    path: path.resolve(__dirname, 'browser')
+  },
+  resolve: {
+    alias: {
+      common: path.resolve(__dirname, 'src/common/')
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.t(bas|pas|py|gx)$/,
+        use: 'raw-loader'
+      }
+    ]
   }
 }
